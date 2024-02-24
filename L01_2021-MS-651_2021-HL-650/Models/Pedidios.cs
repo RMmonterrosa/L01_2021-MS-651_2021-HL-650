81bc8d3336bd0fbb1e3d1f307a3a968f3df6361b
MAmonterrosa <rodrigo.monterrosa@catolica.edu.sv>
2024-02-24 08:55:04 -0600

    Add project files.

---
+++ L01_2021-MS-651_2021-HL-650/Models/Pedidios.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L01_2021_MS_651_2021_HL_650.Models
+{
+    public class Pedidios
+    {
+
+        [Key]
+        public int pedidoId { get; set; }
+        public int motoristaId { get; set; }
+        public int clienteId { get; set; }
+        public int platoId { get; set; }
+        public int? cantidad { get; set; } 
+        public decimal? precio { get; set; }
+
+    }
+}
---
+++ L01_2021-MS-651_2021-HL-650/Models/pruebaContext.cs
@@ -0,0 +1,18 @@
+﻿using L01_2021_MS_651_2021_HL_650.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace Def.Models
+{
+    public class pruebaContext : DbContext
+    {
+
+        public pruebaContext(DbContextOptions<pruebaContext> options) : base(options)  
+        {
+        }
+
+        public DbSet<Pedidios> pedidos {  get; set; } 
+        public DbSet<Summer> Summero { get; set; } 
+
+    }
+}
