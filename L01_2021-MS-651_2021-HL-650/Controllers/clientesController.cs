37139c4a3f79da7d1bea03d4190a241138a90f0c
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2024-02-24 09:47:34 -0600

    Agregado controller de clientes

---
+++ L01_2021-MS-651_2021-HL-650/Controllers/clientesController.cs
@@ -0,0 +1,39 @@
+﻿using L01_2021_MS_651_2021_HL_650.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L01_2021_MS_651_2021_HL_650.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class clientesController : ControllerBase
+    {
+        private readonly pruebaContext _pruebaContext;
+
+        public clientesController(pruebaContext pruebaContext)
+        {
+
+            _pruebaContext = pruebaContext;
+
+        }
+
+        [HttpGet]
+        [Route("GetId/{id}")]
+        public IActionResult Buscar(int id)
+        {
+
+            clientes? listaClientes = (from p in _pruebaContext.clientes
+                                     where p.clienteId == id
+                                     select p).FirstOrDefault();
+
+            if (listaClientes == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(listaClientes);
+
+        }
+    }
+}
