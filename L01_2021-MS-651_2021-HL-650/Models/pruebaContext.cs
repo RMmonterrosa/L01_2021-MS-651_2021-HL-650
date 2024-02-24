d594cecfa3bddff82ad09bfe45f6dfbafe091cba
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2024-02-24 10:02:06 -0600

    Se agregó el controlador de clientes

--- L01_2021-MS-651_2021-HL-650/Models/pruebaContext.cs
+++ L01_2021-MS-651_2021-HL-650/Models/pruebaContext.cs
@@ -15,7 +15,6 @@ namespace L01_2021_MS_651_2021_HL_650.Models
         public DbSet<platos> platos { get; set; }
         public DbSet<motoristas> motoristas { get; set;}
 
-        public DbSet<platos> platos { get; set; }
 
 
 
